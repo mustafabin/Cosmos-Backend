85accc191d9f2f4d7020e307d6297a6ddcc4f57e
mustafabin <mbin828@gmail.com>
2022-04-24 17:56:13 -0400

    changed up the seeding

--- seeding/seed.js
+++ seeding/seed.js
@@ -1,18 +1,11 @@
-import ProductModel from '../models/productModel.js';
-import UserModel from '../models/userModel.js';
-import db from '../connection/connection.js';
-import planetData from '../seeding/data/planetData.json' assert { type: 'json' };
-import userData from '../seeding/data/userData.json' assert {type: 'json'};
-
+import ProductModel from "../models/productModel.js";
+import db from "../connection/connection.js";
+import planetData from "../seeding/data/planetData.json" assert { type: "json" };
 
 const seedDatabase = async () => {
-
   await ProductModel.deleteMany();
-  await UserModel.insertMany(userData);
   await ProductModel.insertMany(planetData);
   db.close();
-
-}
+};
 
 seedDatabase();
-
